6fed4edb4d1eea4853354b5c93ad8abeb61bdae4
brianminges <btminges@gmail.com>
2022-05-26 20:19:38 -0500

    initial commit

---
+++ levelup/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views.game_type import GameTypeView
+from levelupapi.views.event import EventView
+from levelupapi.views.game import GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'events', EventView, 'event')
+router.register(r'games', GameView, 'game')
+
+
+urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
